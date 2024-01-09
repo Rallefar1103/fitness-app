4f3b348fe3b356066881557f3c0758d84e46c3f4
Rallefar1103 <rasmus@turftank.com>
2024-01-08 18:47:46 -0800

    adds background image to the home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,23 @@ import { Box } from "@mui/material";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
+import HeroBannerImg from "../assets/images/fitness-home-pic.jpg";
 
 function Home() {
   return (
-    <Box>
+    <Box
+      width="400px"
+      sx={{
+        width: { xl: "100vw" },
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${HeroBannerImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+        position: "relative",
+        m: "auto",
+      }}
+    >
       <HeroBanner />
       <SearchExercises />
       <Exercises />
