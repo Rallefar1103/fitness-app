bd52fca4046bc14fd47ccd66d6074d18449835e3
Rallefar1103 <rasmus@turftank.com>
2024-01-08 16:26:37 -0800

    first commit - initialising repo and app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Connect application to the DOM
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("id"));
+
+import App from "/App";
+
+root.render(<App />);
