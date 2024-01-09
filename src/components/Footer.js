a220490c0032a0da25c3ca71ab804b3eb2bb1eed
Rallefar1103 <rasmus@turftank.com>
2024-01-09 15:30:27 -0800

    adds the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/Logo.png";
 
-function Footer() {
-  return <div>Footer</div>;
-}
+const Footer = () => (
+  <Box pb="15px" bgcolor="#F3F3F5">
+    <Stack gap="40px" sx={{ alignItems: "center" }} flexWrap="wrap">
+      <img src={Logo} alt="logo" style={{ width: "70px", height: "70px" }} />
+    </Stack>
+    <Typography
+      sx={{ fontSize: { lg: "15px", xs: "15px" } }}
+      textAlign="center"
+    >
+      Fitness Inspo by Rasmus Henriksen
+    </Typography>
+  </Box>
+);
 
 export default Footer;
