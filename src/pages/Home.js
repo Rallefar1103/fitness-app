9eec1f03025d993ccb082a828ad003bedc64194b
Rallefar1103 <rasmus@turftank.com>
2024-01-08 22:37:47 -0800

    reorders component on home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,37 +4,52 @@ import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 import HeroBannerImg from "../assets/images/fitness-home-pic.jpg";
+import ExercisesBannerImg from "../assets/images/fitness-exercises-pic.jpg";
 
 function Home() {
   const [bodyPart, setBodyPart] = useState("all");
   const [exercises, setExercises] = useState([]);
 
   return (
-    <Box
-      width="400px"
-      sx={{
-        width: { xl: "100vw" },
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${HeroBannerImg})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        minHeight: "100vh",
-        position: "relative",
-        m: "auto",
-      }}
-    >
-      <HeroBanner />
-      <SearchExercises
-        setExercises={setExercises}
-        bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
-      />
-      <Exercises
-        setExercises={setExercises}
-        bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
-      />
-    </Box>
+    <>
+      <Box
+        width="400px"
+        sx={{
+          width: { xl: "100vw" },
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${HeroBannerImg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
+          position: "relative",
+        }}
+      >
+        <HeroBanner />
+        <SearchExercises
+          setExercises={setExercises}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
+      </Box>
+      <Box
+        width="400px"
+        sx={{
+          width: { xl: "100vw" },
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${ExercisesBannerImg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
+          position: "relative",
+        }}
+      >
+        <Exercises
+          setExercises={setExercises}
+          bodyPart={bodyPart}
+          exercises={exercises}
+        />
+      </Box>
+    </>
   );
 }
 
