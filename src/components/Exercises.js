d245a9dcde5d6c5ac53ba0aab25548c2569b2a0f
Rallefar1103 <rasmus@turftank.com>
2024-01-08 22:37:01 -0800

    adds the exercises component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
+import { exercisesOptions, fetchData } from "../utils/fetchData";
 
-function Exercises() {
-  return <div>Exercises</div>;
-}
+import ExerciseCard from "./ExerciseCard";
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  return (
+    <Box
+      id="exercises"
+      sx={{
+        mt: { lg: "110px" },
+      }}
+      mt="50px"
+      p="20px"
+    >
+      <Typography variant="h4" mb="45px" color="whitesmoke" textAlign="center">
+        Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{
+          gap: { lg: "110px", xs: "50px" },
+        }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+    </Box>
+  );
+};
 
 export default Exercises;
