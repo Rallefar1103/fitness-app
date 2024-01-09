97f31bbbbfb0d372f0692388e4c3a83676e88aab
Rallefar1103 <rasmus@turftank.com>
2024-01-08 16:51:04 -0800

    changes render id to root

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "/App";
+import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("id"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
