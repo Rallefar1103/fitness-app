8e89b70b4accbe481d1e71be214a3ad719b9183b
Rallefar1103 <rasmus@turftank.com>
2024-01-08 21:03:23 -0800

    adds fetching logic for exercises

---
+++ src/utils/fetchData.js
@@ -0,0 +1,20 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a22ec9fd5cmsh53b25368f39b29fp1ab928jsn6c2604dd639f",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const exercisedbUrl = "https://exercisedb.p.rapidapi.com/exercises";
+
+export const fetchData = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+  } catch (e) {
+    console.log("Error occured fetching data", e);
+  }
+};
