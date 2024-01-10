b4e0454e1920be84d792c183c327cc2be592135d
Rallefar1103 <rasmus@turftank.com>
2024-01-09 17:44:59 -0800

    styles the exercise card component

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,48 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography, Box } from "@mui/material";
 
 const ExerciseCard = ({ exercise }) => {
   return (
     <Link to={`/exercise/${exercise.id}`} className="exercise-card">
       <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
-      <Stack direction="row">
-        <Button
-          sx={{
-            ml: "20px",
-            color: "#FFF",
-            background: "#ffa9a9",
-            fontSize: "10px",
-            borderRadius: "20px",
-            textTransform: "capitalize",
-          }}
-        >
-          {exercise.bodyPart}
-        </Button>
-        <Button
-          sx={{
-            ml: "20px",
-            color: "#FFF",
-            background: "#fcc757",
-            fontSize: "10px",
-            borderRadius: "20px",
-            textTransform: "capitalize",
-          }}
-        >
-          {exercise.target}
-        </Button>
-      </Stack>
-      <Typography
-        ml="23px"
-        color="black"
-        fontWeight="bold"
-        mt="10px"
-        pb="10px"
-        textTransform="capitalize"
-        fontSize="20px"
+      <Box
+        sx={{
+          mt: "25px",
+          pr: "10px",
+        }}
       >
-        {exercise.name}
-      </Typography>
+        <Stack direction="row">
+          <Button
+            sx={{
+              ml: "20px",
+              color: "#FFF",
+              background: "#ff2625",
+              fontSize: "10px",
+              borderRadius: "20px",
+              textTransform: "capitalize",
+            }}
+          >
+            {exercise.bodyPart}
+          </Button>
+          <Button
+            sx={{
+              ml: "20px",
+              color: "#FFF",
+              background: "#FCCF43",
+              fontSize: "10px",
+              borderRadius: "20px",
+              textTransform: "capitalize",
+            }}
+          >
+            {exercise.target}
+          </Button>
+        </Stack>
+        <Typography
+          ml="23px"
+          color="black"
+          fontWeight="bold"
+          mt="10px"
+          pb="10px"
+          textTransform="capitalize"
+          fontSize="17px"
+          pad
+        >
+          {exercise.name}
+        </Typography>
+      </Box>
     </Link>
   );
 };
